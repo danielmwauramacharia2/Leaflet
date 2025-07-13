4ab815a3ecacf3a1d1d1bb9290176d15f1395b89
danielmwauramacharia2 <danielmwaura303@gmail.com>
2025-07-13 16:40:08 +0300

    Add everything of the application

--- Leaflet-Biginners/main.js
+++ Leaflet-Biginners/main.js
@@ -1,3 +1,4 @@
+// Base maps
 var osmMap = L.tileLayer.provider("OpenStreetMap.Mapnik");
 var stamenMap = L.tileLayer.provider("Stadia.StamenWatercolor");
 var imageryMap = L.tileLayer.provider("Esri.WorldImagery");
@@ -5,17 +6,43 @@ var imageryMap = L.tileLayer.provider("Esri.WorldImagery");
 var baseMaps = {
     OSM: osmMap,
     "Stamen Watercolor": stamenMap,
-    "World Imagery": imageryMap
+    "World Imagery": imageryMap,
+};
 
-}
+// Initialize the map
+var map = L.map('map', {
+    center: [-1.286389, 36.817223],
+    zoom: 5,
+    layers: [osmMap]
+});
 
-var map = L.map('map', 
-    {
-        center: [-1.286389, 36.817223],
-        zoom: 5,
-        layers: [osmMap]
+
+// Initialize empty overlays first
+var overlayMaps = {};
+
+// Add layer control immediately
+var layerControl = L.control.layers(baseMaps, overlayMaps, { position: 'topright' }).addTo(map);
+
+// Fetch and add Kenya Counties later
+var kenyaCountyLayer;
+fetch('resources/Kenya_Counties_map.geojson')
+    .then(response => response.json())
+    .then(data => {
+        kenyaCountyLayer = L.geoJSON(data, {
+            style: {
+                // color: '#5e0606ff',
+                weight: 0.5,
+                // fillColor: '#bc62b7ff',
+                // fillOpacity: 0.5,
+                // // fill: false
+            },
+            onEachFeature: function (feature, layer) {
+                if (feature.properties && feature.properties.NAME) {
+                    layer.bindPopup(feature.properties.NAME);
+                }
+            }
+        }).addTo(map);
+
+        // Update overlays AFTER layer is created
+        layerControl.addOverlay(kenyaCountyLayer, "Kenya Counties");
     });
-var mapLayers = L.control.layers(baseMaps).addTo(map);
-
-
-
